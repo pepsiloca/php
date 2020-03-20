0e9c595132167f6da671f9ca27b5fc0bb9cc7660
u8931011 <u8931011@gmail.com>
2020-03-20 16:35:32 +0800

    last commit

--- index.php
+++ index.php
@@ -1,12 +1,54 @@
 <?php
 
-echo "hello world";
+//運算子
+$a=false;
+echo $a;
+echo "<hr>";
+echo !$a;
 
-//ctrl+`
+//運算子 ++
+echo "<hr>";
+$b=1;
+echo $b++;
+echo "<hr>";
+echo $b+1;
 
-$aa;
+echo"<hr>";
+
+//算數運算子+
+
+$a=2;
+$b=4;
+echo $a+$b;
+
+echo "<hr>";
+
+
+
+
+//宣告一個變數
+$a=1;
+echo $a;
+echo "<br>";
+
+$a=2.1;
+echo $a;
+print($a);
+echo
+
+/*
+宣告一個常數不可變更
+*/
+
+define("PI",3.14);
+echo PI;
+echo PI*2*10;
+
+
+$stuentInSchool="yes";
+
+
 
 
 ?>
 
-<h1>hello world</h1>
